bbceebacebedd49a6fcb4fc2439ee7df58701232
ojan <ojanmaharjan03@gmail.com>
2025-09-06 22:28:22 +0545

    update

---
+++ env/hospital_management/Patients/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def Patients(request):
+    return render(request,"patient.html")
+
---
+++ env/hospital_management/hostpial_project/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.template import loader
+
+def home(request):
+        return render(request,"index.html")
+        
+
+
+

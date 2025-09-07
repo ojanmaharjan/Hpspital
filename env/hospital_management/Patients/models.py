dfa1ea1a2b8d4fc5e101a04f953116948c42a1f2
ojan <ojanmaharjan03@gmail.com>
2025-09-07 22:46:14 +0545

    update

--- env/hospital_management/Patients/models.py
+++ env/hospital_management/Patients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    GENDER_CHOICE =[
+        ('Male','Male'),
+        ('Female','Female'),
+        ('other','other')
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(max_length=100)
+    gender = models.CharField(max_length=100,choices=GENDER_CHOICE)
+    contact= models.CharField(max_length=100)
+    address=models.CharField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
--- env/hospital_management/Patients/views.py
+++ env/hospital_management/Patients/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Patient
 
 def Patients(request):
     return render(request,"patient.html")
+
+def PatientInsert(request):
+    if request.method == 'POST':
+        print("from submitteed")
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        contact = request.POST.get('contact')
+        address = request.POST.get('address')
+
+        Patient.objects.create(
+            name=name,
+            age=age,
+            gender=gender,
+            contact=contact,
+            address=address
+        )
+        print("form saved")
+        return redirect('home')
+    return render(request,'Patient.html')
 
